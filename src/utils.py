681991032f8d1479cae883e8c38b02d35e974f75
2514Ajaykumar <ajaynani2514@gmail.com>
2025-03-11 20:09:43 +0530

    Modal Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import dill
 import numpy as np
 
+from sklearn.metrics import r2_score
 from src.exception import CustomException   
 def save_object(obj, file_path):
     '''
@@ -17,3 +18,15 @@ def save_object(obj, file_path):
             dill.dump(obj,file)
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+from sklearn.metrics import r2_score
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    report = {}
+    for model_name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        r2 = r2_score(y_test, y_pred)
+        report[model_name] = r2
+    return report
